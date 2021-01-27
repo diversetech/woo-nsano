ecdded2c95cf414840deab22648aeca0c44fbf25
Gideon Ofori <kgoofori@gmail.com>
2021-01-27 14:26:29 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Nsano WooCommerce Payments Gateway 
+
+## Introduction
+Nsano is aggregation and payments platform.
+
+This Woocommerce payment gateway allows merchants to add Nsano Payments to their WordPress websites and receive card and mobile money payments from their customers.
+
+Sign up for a free [Nsano Merchant Account](https://nsano.com)
+
+
+## Plugin Features
+
+- Accept payments from bank cards (VISA & Mastercard)
+- Accept payments from Ghanainan mobile money channels
+
+
+## Installation 
+
+### Automatic Installation
+- 	Login to your WordPress Admin area
+- 	Go to "Plugins > Add New" from the left hand menu
+- 	Serarch for __Nsano Payments Gateway__
+-	From the search result you will see __Nsano Payments Gateway__ click __Install Now__ to install the plugin
+-	After installation, activate the plugin from the admin dashboard.
+- 	Go to the settings page for WooCommerce and click the "Checkout" tab.
+- 	Click on the __Nsano Payments__ link from the available Checkout Options
+-	Configure your __Nsano Payments__ settings. See below for details.
+
+### Manual Installation
+- 	Download the plugin zip file
+- 	Login to your WordPress Admin. Click on "Plugins > Add New" from the left hand menu.
+-   Click on the "Upload" option, then click "Choose File" to select the zip file from your computer. Once selected, press "OK" and press the "Install Now" button.
+-   Activate the plugin.
+- 	Go to the settings page for WooCommerce and click the "Checkout" tab.
+- 	Click on the __Nsano Payments__ link from the available Checkout Options
+-	Configure your __Nsano Payments__ settings. See below for details.
+
+or
+
+- Unzip the files and upload the folder into your plugins folder (/wp-content/plugins/) overwriting older versions if they exist
+- Activate the plugin in your WordPress admin area.
+
+
+## Configure the plugin
+To configure the Nsano Payments Gateway plugin to work with your WordPress site, go to __WooCommerce > Settings__ from the left hand menu, then click __Checkout__ from the top tab. You will see __Nsano Payments__ as part of the available Checkout Options. Click on it to configure the payment gateway.
+
+- __Enable/Disable__ - check this box to enable you receive payments with Nsano.
+- __Title__ - this displays the title your customers see when they are choosing checkout options on the checkout page.
+- __Description__ - this displays the description under the checkout title on the checkout page.
+- __Merchant ID__ - provide your Nsano [Merchant ID](https://nsano.com/) on your Nsano dashboard.
+- __Merchant Key__ - provide your Nsano Merchant Key](https://nsano.com/) on your Nsano dashboard.
+- Remember to configure your currency to Ghana Cedis on WooCommerce.
+- Set the callback url for your API credetials to (http://your-site.com/wc-api/wc_nsano_payment_callback/)
+
+## Frequently Asked Questions
+
+How do I get started with receiving payments with Nsano using this plugin?
+
+1.	You need to have a Nsano Account.
+2.	You also need to have WooCommerce plugin installed and activated on your WordPress site.
+3.	After this, install and activate this plugin.
+
+What do I do if I am facing issues with this plugin?
+
+You can send an email to admin@wgghana.com
+
+Where do I get more information on how to use Nsano?
+
+You can send an email to admin@wgghana.com
+
+## Changelog
+
+1. First Release
+
+1.0.0 is a initial release of this plugin
---
+++ woo-nsano.php
@@ -0,0 +1,297 @@
+<?php
+/**
+ * Plugin Name: Nsano Woocommerce Payments.
+ * Plugin URI: https://github.com/diversetech/woo-nsano.git
+ * Description: This plugin enables you to accept online payments for cards and mobile money payments using Nsano payment checkout.
+ * Version: 1.0.0
+ * Author: Gideon Ofori
+ * Author URI: http://github.com/kgoofori
+ * Author Email: kgoofori@gmail.com
+ * License: GPLv2 or later
+ * Requires at least: 4.4
+ * Tested up to: 5.2.3
+ * 
+ * 
+ * @package Nsano Payments Gateway
+ * @category Plugin
+ * @author Gideon Ofori
+ */
+
+
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+// if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))){
+//     echo "<div class='error notice'><p>Woocommerce has to be installed and active to use the the Nsano Payments Gateway</b> plugin</p></div>";
+//     return;
+// }
+
+function woo_nsano_init()
+{
+	function add_woo_nsano_payment_gateway( $methods ) 
+	{
+		$methods[] = 'WC_WooNsano_Payment_Gateway'; 
+		return $methods;
+	}
+	add_filter( 'woocommerce_payment_gateways', 'add_woo_nsano_payment_gateway' );
+
+	if(class_exists('WC_Payment_Gateway'))
+	{
+		class WC_WooNsano_Payment_Gateway extends WC_Payment_Gateway 
+		{
+
+			public function __construct()
+			{
+
+				$this->id               = 'woo-nsano-payments';
+				$this->icon             = plugins_url( 'images/nsano-logo.png' , __FILE__ ) ;
+				$this->has_fields       = true;
+				$this->method_title     = 'WooNsano Payments'; 
+				$this->description       = $this->get_option( 'woo_nsano_description');            
+				$this->init_form_fields();
+				$this->init_settings();
+
+				$this->title                    = $this->get_option( 'woo_nsano_title' );
+				$this->woo_nsano_description       = $this->get_option( 'woo_nsano_description');
+				$this->woo_nsano_merchant_id  	    = $this->get_option( 'woo_nsano_merchant_id' );
+				$this->woo_nsano_merchant_key  	    = $this->get_option( 'woo_nsano_merchant_key' );
+
+				
+				if (is_admin()) 
+				{
+
+					if (version_compare(WOOCOMMERCE_VERSION, '2.0.0', '>=')) {
+						add_action('woocommerce_update_options_payment_gateways_' . $this->id, array(&$this, 'process_admin_options'));
+					} else {
+						add_action('woocommerce_update_options_payment_gateways', array(&$this, 'process_admin_options'));
+					}	
+				}
+				
+				//register webhook listener action
+				add_action( 'woocommerce_api_wc_woo_nsano_payment_callback', array( $this, 'check_woo_nsano_payment_webhook' ) );
+
+			}
+
+			public function init_form_fields()
+			{
+
+				$this->form_fields = array(
+					'enabled' => array(
+						'title' =>  'Enable/Disable',
+						'type' => 'checkbox',
+						'label' =>  'Enable Nsano Payments',
+						'default' => 'yes'
+						),
+
+					'woo_nsano_title' => array(
+						'title' =>  'Title',
+						'type' => 'text',
+						'description' =>  'This displays the title which the user sees during checkout options.',
+						'default' =>  'Pay With Nsano',
+						'desc_tip'      => true,
+						),
+
+					'woo_nsano_description' => array(
+						'title' =>  'Description',
+						'type' => 'textarea',
+						'description' =>  'This is the description which the user sees during checkout.',
+						'default' =>  'Safe and secure payments with Ghana issued cards and mobile money from all networks.',
+						'desc_tip'      => true,
+						),
+
+					'woo_nsano_merchant_id' => array(
+						'title' =>  'Merchant ID',
+						'type' => 'text',
+						'description' =>  'This is your Merchant ID which you can find in your Dashboard.',
+						'default' => '',
+						'desc_tip'      => true,
+						'placeholder' => 'Merchant ID'
+						),
+
+					'woo_nsano_merchant_ky' => array(
+						'title' =>  'Merchant Key',
+						'type' => 'text',
+						'description' =>  'This is your Merchant Key which you can find in your Dashboard.',
+						'default' => '',
+						'desc_tip'      => true,
+						'placeholder' => 'Merchant Key'
+						),
+					);
+
+			}
+
+			/**
+			 * handle webhook 
+			 */
+			public function check_woo_nsano_payment_webhook()
+			{
+				$decode_webhook = json_decode(@file_get_contents("php://input"), true);
+
+				$base_url = 'https://manilla.nsano.com/checkout/payment';
+				$response = wp_remote_post($base_url, array(
+					'method' => 'POST',
+					'timeout' => 45,
+					'headers' => array(
+						'Content-Type' => 'application/json',
+						'Accept' => 'application/json',
+						'Authorization' => $this->woo_nsano_merchant_id
+						
+					),
+					'body' => json_encode([
+						"order_id" => $decode_webhook['order_id'],
+						"merchant_apiKey" => $this->woo_nsano_merchant_key
+					])
+					)
+				);
+
+				
+				//retrieve response body and extract the 
+				$response_code = wp_remote_retrieve_response_code( $response );
+				$response_body = wp_remote_retrieve_body($response);
+
+				$response_body_args = json_decode($response_body, true);
+
+				
+
+				global $woocommerce;
+				$order_ref = $decode_webhook['order_id'];
+
+				//retrieve order id from the client reference
+				$order_ref_items = explode('-', $order_ref);
+				$order_id = end($order_ref_items);
+
+				$order = new WC_Order( $order_id );
+				//process the order with returned data from Nsano callback
+
+				if($response_code = 200 && $response_body['code'] == '00')
+				{
+					
+					$order->add_order_note('Nsano payment completed');				
+					
+					//Update the order status
+					$order->update_status('payment processed', 'Payment Successful with Nsano');
+					$order->payment_complete();
+
+					//reduce the stock level of items ordered
+					wc_reduce_stock_levels($order_id);
+				}else{
+					//add notice to order to inform merchant of 
+					$order->add_order_note('Payment failed at Nsano.');
+				}
+
+				echo '12344';
+				
+			}
+
+			/**
+			 * process payments
+			 */
+			public function process_payment($order_id)
+			{
+				global $woocommerce;
+
+				$order = new WC_Order( $order_id );
+
+				// Get an instance of the WC_Order object
+				$order = wc_get_order( $order_id );
+
+				// $order_data = $order->get_items();
+
+				//build order items for the hubel request body
+				$woo_nsano_items = [];
+				$items_counter = 0;
+                $total_cost = 0;
+
+				//Add shipping and VAT as a stand alone item
+				//so that it appears in the customers bill.
+				// $order_shipping_total = $order->get_total_shipping();
+				$order_tax_total = $order->get_total_tax();
+
+				//Nsano payment request body args
+				$woo_nsano_request_args = [
+					  "order_id" => date('YmdHis-').$order_id ,
+					  "description" => $this->get_option('woo_nsano_description'),
+					  "amount" =>  $order->get_total() + $order_tax_total,
+					  "currency" => '',
+					  "return_url" => $order->get_checkout_order_received_url(), //return to this page
+					  "cancel_url" => get_home_url(), //checkout url
+
+					// was double charging shipping
+					// "amount" =>  $order->get_total() + $order_tax_total +  $order_shipping_total,
+					//   "callbackUrl" => WC()->api_request_url( 'WC_WooNsano_Payment_Gateway'), //register callback
+				];
+				
+				
+				//initiate request to Nsano payments API
+				$base_url = 'https://manilla.nsano.com/checkout/payment';
+				$response = wp_remote_post($base_url, array(
+					'method' => 'POST',
+					'timeout' => 45,
+					'headers' => array(
+						'Content-Type' => 'application/json',
+						'Accept' => 'application/json',
+						'Authorization' => $this->woo_nsano_merchant_id
+						
+					),
+					'body' => json_encode($woo_nsano_request_args)
+					)
+				);
+
+				
+				//retrieve response body and extract the 
+				$response_code = wp_remote_retrieve_response_code( $response );
+				$response_body = wp_remote_retrieve_body($response);
+
+				$response_body_args = json_decode($response_body, true);
+
+				switch ($response_code) {
+					case 200:
+							
+						if($response_body['code'] == '00'){
+
+							$woocommerce->cart->empty_cart();
+
+							return array(
+								'result'   => 'success',
+								'redirect' => $response_body_args['data']['redirectUrl']
+							);
+
+						}
+
+						wc_add_notice("HTTP STATUS: {$response_body['code']} - Something went wrong, please try again", "error" );
+						
+							
+						break;
+
+					case 400:
+                        wc_add_notice("HTTP STATUS: $response_code - Payment Request Error: A required field is invalid or empty. Check payment plugin setup.", "error");
+
+						break;
+
+					case 500:
+							wc_add_notice("HTTP STATUS: $response_code - Payment System Error: Contact Nsano for assistance", "error" );
+                            
+						break;
+
+					case 401:
+							wc_add_notice("HTTP STATUS: $response_code - Authentication Error: Request failed due to invalid Nsano credentials. Setup API Key & Secret on your Nsano dashboard", "error" );
+
+						break;
+
+					default:
+							wc_add_notice("HTTP STATUS: $response_code Payment Error: Could not reach Nsano Payment Gateway. Please try again", "error" );
+
+						break;
+				}
+			}
+
+        }  // end of class WC_WooNsano_Payment_Gateway
+
+} // end of if class exist WC_Gateway
+
+}
+
+/*Activation hook*/
+add_action( 'plugins_loaded', 'woo_nsano_init' );
+
+
+
